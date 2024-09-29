ef6d761b6d5ee71b015197cec394b47f97b47a9b
yago <100451322@alumnos.uc3m.es>
2024-09-29 21:59:38 +0200

    fix: script de backup

--- .local/bin/back_up_notes.sh
+++ .local/bin/back_up_notes.sh
@@ -3,11 +3,14 @@
 cd "$HOME/Documentos/"
 
 # Notes directory
-SOURCE_DIR="obsidian-motes"
-BACKUP_FILE="obsidian-motes.zip"
+SOURCE_DIR="obsidian-notes"
+BACKUP_FILE="obsidian-notes.zip"
 
 # Exclude syncthing folder from backup
 zip -r "$BACKUP_FILE" "$SOURCE_DIR" -x ".stfolder/"
-
-# Show notification
-notify-send -i /usr/share/icons/Pop/scalable/apps/disks-symbolic.svg "Backup" "Se ha guardado una copia de las notas"
+if [ $? -eq 0 ]; then
+	notify-send -i /usr/share/icons/Pop/scalable/apps/disks-symbolic.svg "Backup" "Se ha guardado una copia de las notas"
+else 
+	notify-send -i /usr/share/icons/Pop/scalable/apps/preferences-system-notifications-symbolic.svg "Backup" "Fallo al crear la copia de las notas"
+	exit 1
+fi
