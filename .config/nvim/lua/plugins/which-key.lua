f9026bda7515782288f29508fdc76e4837fe5533
yago <100451322@alumnos.uc3m.es>
2024-11-20 19:35:23 +0100

    Empezamos con tmux

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,17 +1,26 @@
 return {
 	"folke/which-key.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons", "echasnovski/mini.icons" },
 	config = function()
 		require("which-key").setup()
 
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ebug", _ = "which_key_ignore" },
-			["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-			["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+		require("which-key").add({
+			{ "<leader>c", group = "[C]ode" },
+			{ "<leader>c_", hidden = true },
+			{ "<leader>d", group = "[D]ebug" },
+			{ "<leader>d_", hidden = true },
+			{ "<leader>g", group = "[G]it" },
+			{ "<leader>g_", hidden = true },
+			{ "<leader>h", group = "Git [H]unk" },
+			{ "<leader>h_", hidden = true },
+			{ "<leader>r", group = "[R]ename" },
+			{ "<leader>r_", hidden = true },
+			{ "<leader>s", group = "[S]earch" },
+			{ "<leader>s_", hidden = true },
+			{ "<leader>t", group = "[T]oggle" },
+			{ "<leader>t_", hidden = true },
+			{ "<leader>w", group = "[W]orkspace" },
+			{ "<leader>w_", hidden = true },
 		})
 		-- register which-key VISUAL mode
 		-- required for visual <leader>hs (hunk stage) to work
--- .local/bin/back_up_notes.sh
+++ .local/bin/back_up_notes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set the display
+export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME pop)/environ)
+
 cd "$HOME/Documentos/"
 
 # Notes directory
@@ -9,8 +12,8 @@ BACKUP_FILE="obsidian-notes.zip"
 # Exclude syncthing folder from backup
 zip -r "$BACKUP_FILE" "$SOURCE_DIR" -x ".stfolder/"
 if [ $? -eq 0 ]; then
-	notify-send -i /usr/share/icons/Pop/scalable/apps/disks-symbolic.svg "Backup" "Se ha guardado una copia de las notas"
+	notify-send -i /usr/share/icons/Pop/scalable/apps/deja-dup-symbolic.svg "Backup" "Se ha guardado una copia de las notas"
 else 
-	notify-send -i /usr/share/icons/Pop/scalable/apps/preferences-system-notifications-symbolic.svg "Backup" "Fallo al crear la copia de las notas"
+	notify-send -i /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup" "Fallo al crear la copia de las notas"
 	exit 1
 fi
