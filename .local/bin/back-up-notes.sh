5e68fd4923149740927e7cabf39b8d43e4e1b85f
yago <100451322@alumnos.uc3m.es>
2026-05-17 13:58:15 +0200

    fix(scripts): los scripts se ejecutan cuando el portatil está apagado 🎉

--- .local/bin/back-up-notes.sh
+++ .local/bin/back-up-notes.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-# Set the display
-export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME pop)/environ)
+# Notification helper: never fail the backup if desktop notifications are unavailable
+notify() {
+	local icon="$1"
+	local title="$2"
+	local body="$3"
+
+	XDG_RUNTIME_DIR="/run/user/$(id -u)" \
+	DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus" \
+		notify-send -i "$icon" "$title" "$body" >/dev/null 2>&1 || true
+}
 
 cd "$HOME/Documentos/"
 
@@ -14,17 +22,17 @@ BACKUP_FILE="obsidian-notes.zip"
 # Exclude syncthing folder from backup
 zip -r "$BACKUP_FILE" "$SOURCE_DIR" -x ".stfolder/"
 if [ $? -eq 0 ]; then
-	notify-send -i /usr/share/icons/Pop/scalable/apps/deja-dup-symbolic.svg "Backup - Notas" "Se ha guardado una copia de las notas"
-else 
-	notify-send -i /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup - Notas" "Fallo al crear la copia de las notas"
+	notify /usr/share/icons/Pop/scalable/apps/deja-dup-symbolic.svg "Backup - Notas" "Se ha guardado una copia de las notas"
+else
+	notify /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup - Notas" "Fallo al crear la copia de las notas"
 	exit 1
 fi
 
 # Subir a drive
 rclone copy "$BACKUP_FILE" yago-drive:Y --progress
 if [ $? -eq 0 ]; then
-	notify-send -i /usr/share/icons/Pop/scalable/places/folder-remote-symbolic.svg "Backup - Notas" "Backup de las notas subido a Google Drive"
+	notify /usr/share/icons/Pop/scalable/places/folder-remote-symbolic.svg "Backup - Notas" "Backup de las notas subido a Google Drive"
 else
-	notify-send -i /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup - Notas" "Fallo al subir el backup a Google Drive"
+	notify /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup - Notas" "Fallo al subir el backup a Google Drive"
 	exit 1
 fi
--- .local/bin/back-up-pswd.sh
+++ .local/bin/back-up-pswd.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-# Set the display
-export $(grep -E -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u "$LOGNAME" pop)/environ)
+# Notification helper: never fail the backup if desktop notifications are unavailable
+notify() {
+	local icon="$1"
+	local title="$2"
+	local body="$3"
+
+	XDG_RUNTIME_DIR="/run/user/$(id -u)" \
+	DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus" \
+		notify-send -i "$icon" "$title" "$body" >/dev/null 2>&1 || true
+}
 
 DB="$HOME/Documentos/keepass/Contraseñas.kdbx"
 GPG_PASS=$(pass show keepass/drive)
@@ -15,9 +23,9 @@ fi
 
 gpg -c --no-symkey-cache --batch --passphrase "$GPG_PASS" --cipher-algo AES256 "$DB"
 if [ $? -eq 0 ]; then
-	notify-send -i /usr/share/icons/Pop/scalable/actions/system-lock-screen-symbolic.svg "Backup - Contraseñas" "Se ha cifrado el almacén de contraseñas"
+	notify /usr/share/icons/Pop/scalable/actions/system-lock-screen-symbolic.svg "Backup - Contraseñas" "Se ha cifrado el almacén de contraseñas"
 else 
-	notify-send -i /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup - Contraseñas" "Fallo al cifrar el almacén de contraseñas"
+	notify /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup - Contraseñas" "Fallo al cifrar el almacén de contraseñas"
 	exit 1
 fi
 
@@ -30,17 +38,17 @@ ORIG_HASH=$(sha256sum "$DB" | awk '{print $1}')
 NEW_HASH=$(gpg --decrypt  --quiet --batch --passphrase "$GPG_PASS" --cipher-algo AES256 "$BACKUP_FILE" | sha256sum | awk '{print $1}')
 
 if [ "$ORIG_HASH" == "$NEW_HASH" ]; then
-	notify-send -i /usr/share/icons/Pop/scalable/actions/selection-checked-symbolic.svg "Backup - Contraseñas" "Se ha verificado el cifrado de las contraseñas"
+	notify /usr/share/icons/Pop/scalable/actions/selection-checked-symbolic.svg "Backup - Contraseñas" "Se ha verificado el cifrado de las contraseñas"
 else
-	notify-send -i /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup - Contraseñas" "Fallo al verificar el cifrado de las contraseñas"
+	notify /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup - Contraseñas" "Fallo al verificar el cifrado de las contraseñas"
 	exit 1
 fi
 
 # Subir a drive
 rclone copy "$BACKUP_FILE" yago-drive:Y --progress
 if [ $? -eq 0 ]; then
-	notify-send -i /usr/share/icons/Pop/scalable/places/folder-remote-symbolic.svg "Backup - Contraseñas" "Backup de las contraseñas subido a Google Drive"
+	notify /usr/share/icons/Pop/scalable/places/folder-remote-symbolic.svg "Backup - Contraseñas" "Backup de las contraseñas subido a Google Drive"
 else
-	notify-send -i /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup - Contraseñas" "Fallo al subir el backup a Google Drive"
+	notify /usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "Backup - Contraseñas" "Fallo al subir el backup a Google Drive"
 	exit 1
 fi
